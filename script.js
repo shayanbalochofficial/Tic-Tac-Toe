06d73cbab1362ffe513fb894ce668a20a4b30b96
Shayan Baloch <shayanbalochofficial@gmail.com>
2026-02-07 08:25:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const boxEls = document.querySelectorAll(".box");
+const statusEl = document.querySelector(".status");
+const restartBtnEl = document.querySelector(".restartBtn");
+
+let x = "<img src='x-letter.svg'>";
+let o = "<img src='o-letter.svg'>";
+
+//Winning combination
+const win = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let options = "";
